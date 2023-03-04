a4c38bd7a7bd35655279ba4e583325f5968030aa
olga <yep4yep@gmail.com>
2023-03-05 00:09:26 +0800

    Домашнее задание №1

---
+++ src/main/java/com/example/playlistmaker/library.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class library : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+//zxczxc
+    }
+}
